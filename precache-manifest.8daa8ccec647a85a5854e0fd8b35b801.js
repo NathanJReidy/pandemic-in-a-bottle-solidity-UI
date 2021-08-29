8c4afc54dccee99d3f6c0c2eb2ff9e154ce30264
Nathan Reidy <nathanjreidy@gmail.com>
2021-08-29 19:12:31 +1000

    Updates

---
+++ precache-manifest.8daa8ccec647a85a5854e0fd8b35b801.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e6d1b7ee03d83ba1a984ab62c260ac1",
+    "url": "/pandemic-in-a-bottle-solidity-UI/index.html"
+  },
+  {
+    "revision": "d9e433ce0e1652257893",
+    "url": "/pandemic-in-a-bottle-solidity-UI/static/css/main.5aebb444.chunk.css"
+  },
+  {
+    "revision": "ee064d480d8c37d33300",
+    "url": "/pandemic-in-a-bottle-solidity-UI/static/js/2.5f5d4044.chunk.js"
+  },
+  {
+    "revision": "d9e433ce0e1652257893",
+    "url": "/pandemic-in-a-bottle-solidity-UI/static/js/main.89d51f2d.chunk.js"
+  },
+  {
+    "revision": "26b5477b1559e52dc334",
+    "url": "/pandemic-in-a-bottle-solidity-UI/static/js/runtime-main.3f1f26c3.js"
+  },
+  {
+    "revision": "de87c146b97f8c6280763345aaaf661b",
+    "url": "/pandemic-in-a-bottle-solidity-UI/static/media/seabottle.de87c146.jpg"
+  }
+]);
